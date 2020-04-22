93a49fdc34bfc88c08e566627ddbe33346a29eb5
chuthaluck <chuthaluck@chuthalucks-Air.vc.shawcable.net>
2020-04-21 22:00:29 -0700

    add new gatsby blog code

---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+
+    <h1>HELLO FROM THIRD PAGE</h1>
+
+  </Layout>
+)
+
+export default ThirdPage
